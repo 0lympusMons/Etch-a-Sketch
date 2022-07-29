06663cd578630a5e259f830d1380ff650ccd860a
0lympusMons <dodgeynohangelomadula2002@gmail.com>
2022-07-29 15:51:37 +0800

    Add true/false when clicking eraser btn

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function generateBox(num) {
     for (let i = 0; i < pixels; i++) {
         let box = document.createElement('div');
         box.classList.add('boxes__container--children');
-        
+
 
         // sets dimensions
         let boxesContainerHeight = boxesContainer.offsetHeight;
         let boxesContainerWidth = boxesContainer.offsetWidth;
 
-        box.style.width = `${boxesContainerWidth/num}px`;
-        box.style.height = `${boxesContainerHeight/num}px`;
+        box.style.width = `${boxesContainerWidth / num}px`;
+        box.style.height = `${boxesContainerHeight / num}px`;
 
         //adds click event listener
         box.addEventListener('click', paint);
@@ -29,9 +29,17 @@ function paint() {
     this.classList.add('black');
 }
 
-function erase(){
+function erase() {
+
+    if (eraseButtonActivated === false) {
+        eraseButtonActivated = true;
+    } else if (eraseButtonActivated === true) {
+        eraseButtonActivated = false;
+    }
+    
     this.classList.add('white');
-    console.log("Does not work");
+    console.log(eraseButtonActivated);
+
 }
 
 
@@ -39,7 +47,11 @@ let boxesContainer = document.querySelector('.boxes__container');
 
 // Settings
 let eraseButton = document.querySelector('.erase_button');
+let eraseButtonActivated = false;
 eraseButton.addEventListener('click', erase);
+
+let clearButton = document.querySelector('.clear_button');
+clearButton.addEventListener('click', generateBox);
 
 generateBox(16);
 
